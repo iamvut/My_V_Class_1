5029e945549536782a2241029994da0721865672
vut <saravut.k@gmail.com>
2016-06-04 11:40:06 +0700

    v_class_01

---
+++ app/src/main/java/com/kgamail/saravut/my_v_class_1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kgamail.saravut.my_v_class_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+ }
